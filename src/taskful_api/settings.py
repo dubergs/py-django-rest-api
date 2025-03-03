d8c5a2257932265bcec4cbf4312a0ccbc6554b47
Duber <gutierrezduber12@gmail.com>
2025-03-03 16:12:23 -0500

    creacion del modelo para los usuarios

--- src/taskful_api/settings.py
+++ src/taskful_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k8qb2=a6!@bm-9(0^ibyfpke0jiqzz!c2xiszy%*+vuj&5mk1!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
