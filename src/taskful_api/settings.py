55fbdbf019425d5aff24df9dc847e45625981c0d
Duber <gutierrezduber12@gmail.com>
2025-03-14 18:17:41 -0500

    Adicion de autenticacion de sesion

--- src/taskful_api/settings.py
+++ src/taskful_api/settings.py
@@ -105,7 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 
