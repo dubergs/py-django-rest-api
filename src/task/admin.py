69d3132a287a85d1e899abdb7d49bcb5c8f64855
Duber <gutierrezduber12@gmail.com>
2025-04-01 09:31:51 -0500

    Creacion de lista de tareas y modelo de archivos adjuntos

--- src/task/admin.py
+++ src/task/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Task, TaskList, Attachment
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(TaskList)
+admin.site.register(Attachment)
